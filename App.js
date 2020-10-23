f6047a6c2bb76b0464da1353df695f6e082e03c8
saadet göçmen <saadettgcmn@gmail.com>
2020-10-23 17:03:03 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button ,Text, View, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.yazi}>Hoş Geldiniz</Text>
+
+      <StatusBar style="light"/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex:1,
+    backgroundColor: '#ff6',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  yazi: {
+  
+  }
 });
