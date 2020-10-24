d16b0754a521639051387cacfc7b65a882cc6b71
hasancankucuk <hasancankucuk@yandex.com>
2020-10-24 23:30:24 +0300

    Galeri Eklendi

--- App.js
+++ App.js
@@ -1,40 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Button ,Text, TextInput, View, Alert } from 'react-native';
+import Gallery from './components/Gallery.js'
 
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.yazi}>Hoş Geldiniz</Text>
-      <Button
-        color="blue"
-        title="Başlamak için buraya tıklayın"
-        onPress={() => Alert.alert('Başarılı')}
-      />
-      <Text>Adınızı yazınız</Text>
-      <TextInput
-      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-      onChangeText={text => onChangeText(text)}
-      value={value}
-    />
-      <StatusBar style="dark"/>
-      
-    </View>
-  );
+class App extends React.Component{
+  render() {
+    return (
+      <Gallery/>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex:1,
-    backgroundColor: 'skyblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  yazi: {
-   fontSize:50,
-   fontWeight:'bold',
-   color:'black',
-
-  }
-});
+export default App;
