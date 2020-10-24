0ca67c4e7974b97dd9af6e84f50ed5686c1422c8
saadet göçmen <saadettgcmn@gmail.com>
2020-10-24 16:49:38 +0300

    mobil uygulama tasarımına başlandı

--- App.js
+++ App.js
@@ -1,17 +1,40 @@
-import { Camera } from 'expo-camera';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import Gallery from './components/Gallery.js';
-import Photo from './components/Photo.js';
+import { StyleSheet, Button ,Text, TextInput, View, Alert } from 'react-native';
 
-class App extends React.Component{
-  render() {
-    return (
-      <>
-        <Gallery/>
-        <Photo/>
-      </>
-    );
-  }
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.yazi}>Hoş Geldiniz</Text>
+      <Button
+        color="blue"
+        title="Başlamak için buraya tıklayın"
+        onPress={() => Alert.alert('Başarılı')}
+      />
+      <Text>Adınızı yazınız</Text>
+      <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => onChangeText(text)}
+      value={value}
+    />
+      <StatusBar style="dark"/>
+      
+    </View>
+  );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: 'skyblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  yazi: {
+   fontSize:50,
+   fontWeight:'bold',
+   color:'black',
+
+  }
+});
